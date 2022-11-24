969d433521d5a3550dad5ce81ad16dba6a6944c6
hxuser <1564423474@qq.com>
2022-11-24 22:45:47 +0800

    第二天学习c进度

---
+++ text_01.c
@@ -0,0 +1,57 @@
+/*
+数据类型
+计算机语言 - 写程序 - 解决生活中的问题
+必须有能力来描述生活中的问题！
+eg：购物商城 - 上架商品，价格 - 15.6元  - 小数  1.56*10^1
+eg 年龄  - 50岁
+c语言 - 浮点数
+      - 整型
+*/
+
+//a
+//'a' - 字符a
+
+/*
+int main()
+{
+	//字符类型
+	char ch = 'a';
+	//整型
+	int age = 20;
+	//短整型
+	short num = 10;
+	//long 长整型
+	//long long 长长整型
+
+	//单精度浮点型
+	float weight = 55.5;
+	//双精度浮点型
+	double d = 0.0;
+
+	return 0;
+}*/
+
+/*打印一个int整型 - %d
+  打印一个float单精度浮点型 - %f
+  打印一个double双精度浮点型 - %lf */
+
+/*
+int main()
+{
+	//printf("hehe\n");
+	//printf("c语言\n");
+	//printf("%d\n", 100);   //打印一个整型 - %d
+	//sizeof - 关键字 - 操作符 - 计算类型或变量所占空间的大小
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+
+	return 0;
+}
+*/
+
+// int g_val = 2022;
